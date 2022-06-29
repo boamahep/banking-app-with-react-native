f1eb8bbb0269c7e1cd069a2aac05b24e875d9c58
Emmanuel Preko Boamah <30542180+boamahep@users.noreply.github.com>
2022-06-29 13:03:40 +0000

    Updated Version 2
    My first react-native app

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,34 +1,12 @@
-import React from 'react';
-import { Image, ImageBackground, StyleSheet, Text, View, Pressable} from 'react-native';
+import React, { Component } from 'react';
+import { Image, StyleSheet, Text, View, Pressable} from 'react-native';
+import Inputs from '../components/Login.js';
 
 function LoginScreen({navigation}) {
     return (
-      <View
-      style={styles.container}
-      >
-      <Image 
-      style={styles.myimage}
-      source={require('../assets/image.png')}
-      >
-      </Image>
-       <ImageBackground 
-       style={styles.background}
-       source={require('../assets/shape.png')}
-       >
-       </ImageBackground>
-       <Image 
-      style={styles.logo}
-      source={require('../assets/logo.png')}
-      ></Image>
 
-  <Text style={styles.myTitle}>
-LOGIN PAGE
-  </Text>
-  
-    <Pressable style={styles.button} onPress={() => navigation.navigate('Welcome')}>
-   <Text style={styles.loginText}> LOGIN </Text>
-    </Pressable>
-       </View>
+      <inputs />
+      
     );
 }
 
@@ -37,34 +15,15 @@ const styles = StyleSheet.create({
 backgroundColor: 'blueviolet',
 flex: 1,
   },
- background: {
-  flex: 1,
- },
- myimage: {
-  width: 500,
-    height: 500,
-    resizeMode: 'center',
-    position: 'absolute',
-    marginTop: 140,
- },
  logo: {
   resizeMode: 'contain',
   position: 'absolute',
-  marginLeft: 300,
+  marginLeft: 150,
   marginTop: 10,
   width: 80,
   height: 80,
   },
-myTitle: {
-  flex: 0.5,
-   position: 'absolute',
-    textDecorationColor: '#0000ff',
-    textAlign: 'center', // <-- the magic
-    fontWeight: 'bold',
-    fontSize: 30,
-    marginTop: 680,
-    marginLeft: 100,
-},
+
 myText: {
   flex: 0.5,
    position: 'absolute',
